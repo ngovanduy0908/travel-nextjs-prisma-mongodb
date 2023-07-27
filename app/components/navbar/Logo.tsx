bc4be7b5a8136fc02b0faaedbd7f67d29bd739c1
ngovanduy0908 <ngoduy090801@gmail.com>
2023-07-27 16:38:57 +0700

    dev-fix-logo

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -3,8 +3,10 @@ import React from "react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 const Logo = () => {
+  const router = useRouter();
   return (
     <Image
+      onClick={() => router.push("/")}
       alt="logo"
       className="hidden md:block cursor-pointer"
       height="100"
